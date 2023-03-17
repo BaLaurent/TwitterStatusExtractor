d79b1d722cf3ca6d0b06fa7467090335be5b6589
BLaurent <d3adlol0@gmail.com>
2023-03-17 15:05:09 +0100

    Update tse.py

--- tse.py
+++ tse.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 def getStatusIds(user,nbStatus=3,headless=True):
